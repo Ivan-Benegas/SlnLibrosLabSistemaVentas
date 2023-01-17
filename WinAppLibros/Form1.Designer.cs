e433ba1c397fad1d67a807f57155377f1101743e
Ivan Benegas <ivanbenegas1996@gmail.com>
2023-01-17 08:03:25 -0300

    Agregar archivos de proyecto.

---
+++ WinAppLibros/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace WinAppLibros
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelAutor = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelAutor
+            // 
+            this.labelAutor.AutoSize = true;
+            this.labelAutor.Location = new System.Drawing.Point(290, 161);
+            this.labelAutor.Name = "labelAutor";
+            this.labelAutor.Size = new System.Drawing.Size(54, 13);
+            this.labelAutor.TabIndex = 0;
+            this.labelAutor.Text = "labelAutor";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelAutor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelAutor;
+    }
+}
+
---
+++ WinAppLibros/Form1.cs
@@ -0,0 +1,43 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinAppLibros
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Autor autor = new Autor();
+
+            autor.Nombre = "Juan";
+            autor.Apellido = "González";
+            autor.FechaNacimiento = new DateTime(1980, 01, 10);
+            autor.Domicilio = "Avenida Genial 123";
+            autor.Ciudad = "Rosario";
+            autor.Pais = "Argentina";
+
+            labelAutor.Text =
+                "El nombre del autor es " + autor.Nombre + "\n" +
+                "El apellido es " + autor.Apellido + "\n" +
+                "La fecha de nacimiento es " + autor.FechaNacimiento + "\n" +
+                "El domicilio es " + autor.Domicilio + "\n" +
+                "La ciudad es " + autor.Ciudad + "\n" +
+                "El país es " + autor.Pais;
+
+        }
+
+    }
+}
